0fb97b49849331a32acf8229aba7aebd74faea1c
mickael <mickael@soloso.fr>
2022-03-11 11:08:20 +0200

    [firebase.js] modification in the firebase variables file to avoid having in plain text the firebase config. It is a matter of security of the project

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -1,16 +1,29 @@
- //import firebase   from 'firebase'
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth'
-  const firebaseConfig = {
-    apiKey: "AIzaSyDq9SUlRQhU5Rtljg0s2wfCVKgAOT-420M",
-    authDomain: "startup-commons.firebaseapp.com",
-    databaseURL: "https://startup-commons.firebaseio.com",
-    projectId: "startup-commons",
-    storageBucket: "startup-commons.appspot.com",
-    messagingSenderId: "799010509327",
-    appId: "1:799010509327:web:78cc1dfae9095af579497a"
-  };
+//import firebase   from 'firebase'
+import {initializeApp} from 'firebase/app';
+import {getAuth} from 'firebase/auth'
 
-const app=initializeApp(firebaseConfig);
+const {
+    REACT_APP_DEV_API_KEY,
+    REACT_APP_DEV_AUTH_DOMAIN,
+    REACT_APP_DEV_DATABASE_URL,
+    REACT_APP_DEV_PROJECT_ID,
+    REACT_APP_DEV_STORAGE_BUCKET,
+    REACT_APP_DEV_MESSAGING_SENDER_ID,
+    REACT_APP_DEV_DEV_APP_ID,
+    REACT_APP_DEV_DEV_MEASUREMENT_ID
+} = process.env;
+
+const firebaseConfig = {
+    apiKey: REACT_APP_DEV_API_KEY,
+    authDomain: REACT_APP_DEV_AUTH_DOMAIN,
+    databaseURL: REACT_APP_DEV_DATABASE_URL,
+    projectId: REACT_APP_DEV_PROJECT_ID,
+    storageBucket: REACT_APP_DEV_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_DEV_MESSAGING_SENDER_ID,
+    appId: REACT_APP_DEV_DEV_APP_ID,
+    measurementId: REACT_APP_DEV_DEV_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-export {app ,auth} 
+export {app, auth} 
