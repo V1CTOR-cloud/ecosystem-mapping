46dc2273273345815bc5dbbb37de1031cf1de46a
mickael <mickael@soloso.fr>
2022-03-15 10:51:41 +0200

    [firebase] renaming the environment variables names to be more generic for the alpha and beta version

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -3,25 +3,25 @@ import {initializeApp} from 'firebase/app';
 import {getAuth} from 'firebase/auth'
 
 const {
-    REACT_APP_DEV_API_KEY,
-    REACT_APP_DEV_AUTH_DOMAIN,
-    REACT_APP_DEV_DATABASE_URL,
-    REACT_APP_DEV_PROJECT_ID,
-    REACT_APP_DEV_STORAGE_BUCKET,
-    REACT_APP_DEV_MESSAGING_SENDER_ID,
-    REACT_APP_DEV_DEV_APP_ID,
-    REACT_APP_DEV_DEV_MEASUREMENT_ID
+    REACT_APP_API_KEY,
+    REACT_APP_AUTH_DOMAIN,
+    REACT_APP_DATABASE_URL,
+    REACT_APP_PROJECT_ID,
+    REACT_APP_STORAGE_BUCKET,
+    REACT_APP_MESSAGING_SENDER_ID,
+    REACT_APP_APP_ID,
+    REACT_APP_MEASUREMENT_ID
 } = process.env;
 
 const firebaseConfig = {
-    apiKey: REACT_APP_DEV_API_KEY,
-    authDomain: REACT_APP_DEV_AUTH_DOMAIN,
-    databaseURL: REACT_APP_DEV_DATABASE_URL,
-    projectId: REACT_APP_DEV_PROJECT_ID,
-    storageBucket: REACT_APP_DEV_STORAGE_BUCKET,
-    messagingSenderId: REACT_APP_DEV_MESSAGING_SENDER_ID,
-    appId: REACT_APP_DEV_DEV_APP_ID,
-    measurementId: REACT_APP_DEV_DEV_MEASUREMENT_ID
+    apiKey: REACT_APP_API_KEY,
+    authDomain: REACT_APP_AUTH_DOMAIN,
+    databaseURL: REACT_APP_DATABASE_URL,
+    projectId: REACT_APP_PROJECT_ID,
+    storageBucket: REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+    appId: REACT_APP_APP_ID,
+    measurementId: REACT_APP_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
