b42c2d516516205d3978f8caf976588859a0dd97
mickael <mickael@soloso.fr>
2022-03-15 10:52:11 +0200

    [firebase] remove unused import

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -1,29 +1,28 @@
-//import firebase   from 'firebase'
-import {initializeApp} from 'firebase/app';
-import {getAuth} from 'firebase/auth'
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const {
-    REACT_APP_API_KEY,
-    REACT_APP_AUTH_DOMAIN,
-    REACT_APP_DATABASE_URL,
-    REACT_APP_PROJECT_ID,
-    REACT_APP_STORAGE_BUCKET,
-    REACT_APP_MESSAGING_SENDER_ID,
-    REACT_APP_APP_ID,
-    REACT_APP_MEASUREMENT_ID
+  REACT_APP_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_DATABASE_URL,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESSAGING_SENDER_ID,
+  REACT_APP_APP_ID,
+  REACT_APP_MEASUREMENT_ID,
 } = process.env;
 
 const firebaseConfig = {
-    apiKey: REACT_APP_API_KEY,
-    authDomain: REACT_APP_AUTH_DOMAIN,
-    databaseURL: REACT_APP_DATABASE_URL,
-    projectId: REACT_APP_PROJECT_ID,
-    storageBucket: REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
-    appId: REACT_APP_APP_ID,
-    measurementId: REACT_APP_MEASUREMENT_ID
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  databaseURL: REACT_APP_DATABASE_URL,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
+  measurementId: REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-export {app, auth} 
+export { app, auth };
