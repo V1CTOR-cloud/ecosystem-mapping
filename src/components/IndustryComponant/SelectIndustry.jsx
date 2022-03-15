887ed7cf7fd280bf8e4f424b23c0741483bfdbe9
mickael <mickael@soloso.fr>
2022-03-15 14:23:01 +0200

    [SelectSubIndustryComponent, SelectIndustry] renaming the file, refactoring: naming, default export, import order

--- src/components/IndustryComponant/SelectIndustry.jsx
+++ src/components/IndustryComponant/SelectIndustry.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 import { SimpleGrid } from "@chakra-ui/react";
 
-import IndustrySelectComponent from "./IndustrySelectComponent";
-import { SubIndustryselectComponent } from "./SubIndustryselectComponent";
+import SelectIndustryComponent from "./SelectIndustryComponent";
+import SelectSubIndustryComponent from "./SelectSubIndustryComponent";
 
 const SelectIndustry = (props) => {
   return (
     <SimpleGrid columns={2} spacing={4}>
-      <IndustrySelectComponent
+      <SelectIndustryComponent
         data={props.industries}
         industry={props.locationData.industry}
         selectedIndustry={(s) => props.createMapCallback("industry", s)}
       />
-      <SubIndustryselectComponent
+      <SelectSubIndustryComponent
         data={props.subIndustries}
         subIndustry={props.locationData.subIndustry}
         selectedSubIndustry={(s) => props.createMapCallback("subIndustry", s)}
