7158ddb2a68466be230a8c254fd5db765c395a7b
mickael <mickael@soloso.fr>
2022-03-16 14:33:09 +0200

    [LocationDropdown] Refactoring the LocationDropdown component: import order.

--- src/components/miscellaneousComponents/LocationDropdown.jsx
+++ src/components/miscellaneousComponents/LocationDropdown.jsx
@@ -1,24 +1,22 @@
-import React from 'react'
-import  Location  from 'assets/locations.json';
-import { Select } from '@chakra-ui/react';
+import React from "react";
+
+import { Select } from "@chakra-ui/react";
+
+import Location from "assets/locations.json";
 
 class LocationDropdown extends React.Component {
-    
-    render(){
-        
-        return(
-            <div>
-                <Select className="fm-ip-flds" maxWidth="656px">
-                    <option>Select Location</option>
-                    {
-                        Location.location.map((result)=>(<option text={result.id}>{result.name}</option>))
-                    }
-                </Select>
-            </div>
-        )
-    }
-    
-    
-    
+  render() {
+    return (
+      <div>
+        <Select className="fm-ip-flds" maxWidth="656px">
+          <option>Select Location</option>
+          {Location.location.map((result) => (
+            <option text={result.id}>{result.name}</option>
+          ))}
+        </Select>
+      </div>
+    );
+  }
 }
+
 export default LocationDropdown;
