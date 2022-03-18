389b50989c4c80b1da70d7c3c65f003463e787d9
mickael <mickael@soloso.fr>
2022-03-18 12:31:43 +0200

    [ApplicationType] Refactored ApplicationType component: import order, spacing, default export

--- src/components/serviceComponents/ApplicationType.jsx
+++ src/components/serviceComponents/ApplicationType.jsx
@@ -1,17 +1,24 @@
 import React from "react";
+
 import { FormControl, FormLabel } from "@chakra-ui/react";
-import RadioButtonsApplicationType from "./RadioButtonsApplicationType";
 import { useTranslation } from "react-i18next";
 
-const ApplicationType = ({radioValue,val}) => {
+import RadioButtonsApplicationType from "./RadioButtonsApplicationType";
+
+const ApplicationType = ({ radioValue, val }) => {
   const { t } = useTranslation();
+
   return (
     <FormControl mt={4}>
       <FormLabel className="frm-lbl" mt="24px" mb="17px">
-      {t('startup.popup.service.content.application.type')}
+        {t("startup.popup.service.content.application.type")}
       </FormLabel>
-      <RadioButtonsApplicationType radioValue={(e)=>radioValue(e)} val={val} />
+      <RadioButtonsApplicationType
+        radioValue={(e) => radioValue(e)}
+        val={val}
+      />
     </FormControl>
   );
 };
-export { ApplicationType };
+
+export default ApplicationType;
--- src/components/serviceComponents/ServiceBasicInfo.jsx
+++ src/components/serviceComponents/ServiceBasicInfo.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { ServiceType } from "./ServiceType";
-import { ApplicationType } from "./ApplicationType";
+import ApplicationType from "./ApplicationType";
 import { useTranslation } from "react-i18next";
 import {
   FormControl,
