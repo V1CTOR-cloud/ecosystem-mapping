1e8cd10acde28e262ef9ee98f883a9be33283d42
mickael <mickael@soloso.fr>
2022-03-25 11:00:00 +0200

    [constant] Add the image of save in draft and publish + add their style to the constant

--- src/helper/constant.jsx
+++ src/helper/constant.jsx
@@ -1,2 +1,25 @@
+import publishImage from "../assets/images/service published graphic (1).png";
+import draftImage from "../assets/images/Save time and effort 1 (1).png";
+
 export const financeProfessional = require ("assets/images/In-HouseFinanceProfessional12.png");
 export const SliderButton = require ("assets/images/Ellipse.png");
+
+export const constantPublishImage = publishImage;
+export const constantDraftImage = draftImage;
+
+export const constantPublishImageStyle = {
+    height: "auto",
+    width: "383.09px",
+    margin: "0 auto",
+    top: "55.86px",
+    align: "center",
+};
+
+export const constantDraftImageStyle = {
+    height: "auto",
+    width: "399.4375915527344px",
+    margin: "0 auto",
+    top: "55.108154296875px",
+    borderRadius: "0px",
+    align: "center",
+};
