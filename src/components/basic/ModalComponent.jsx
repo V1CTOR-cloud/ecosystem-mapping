440f12f89fae2f5207661031bc888989bbb62519
mickael <mickael@soloso.fr>
2022-03-29 12:41:39 +0300

    [ModalComponent] Add styled component

--- src/components/basic/ModalComponent.jsx
+++ src/components/basic/ModalComponent.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import {
+  chakra,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -8,15 +9,23 @@ import {
   ModalOverlay,
 } from "@chakra-ui/react";
 
+const ModalContentStyled = chakra(ModalContent, {
+  baseStyle: {
+    height: "640px",
+    maxWidth: "720px !important",
+    borderRadius: "4px",
+  },
+});
+
 //Component that display a Chakra UI modal with a close button and a content that is the children passed as arguments.
 const ModalComponent = (props) => {
   return (
     <Modal isOpen={props.isOpen} onClose={props.onClose} isCentered>
       <ModalOverlay />
-      <ModalContent className="md-ntf">
+      <ModalContentStyled>
         <ModalCloseButton />
         <ModalBody pb={6}>{props.children}</ModalBody>
-      </ModalContent>
+      </ModalContentStyled>
     </Modal>
   );
 };
