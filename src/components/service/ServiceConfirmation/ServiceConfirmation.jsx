741ba6adbaac1cfd1de83c8c3038833a6a105961
mickael <mickael@soloso.fr>
2022-03-29 12:39:57 +0300

    [ServiceConfirmation] Modification in the component: add button component and styled component

--- src/components/service/ServiceConfirmation/ServiceConfirmation.jsx
+++ src/components/service/ServiceConfirmation/ServiceConfirmation.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 
-import { useDisclosure, Button, Box, Image, Container } from "@chakra-ui/react";
+import { Box, Image, useDisclosure } from "@chakra-ui/react";
+import styled from "styled-components";
 
 import ModalComponent from "../../basic/ModalComponent";
+import ButtonComponent from "../../basic/Button";
+
+const TitleModal = styled.h1`
+  font-family: Ubuntu, sans-serif;
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 45px;
+  letter-spacing: 0.02em;
+  text-align: center;
+  color: #222c2f;
+`;
+
+const ContentModal = styled.h1`
+  font-family: Ubuntu, sans-serif;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 22px;
+  text-align: center;
+  color: #222c2f;
+`;
 
 // Component that display a button, if clicked, it will show a modal with a title, content and image.
 // Used mainly for: publishing a service; saving in draft a service.
@@ -18,19 +41,20 @@ const ServiceConfirmation = (props) => {
 
   return (
     <>
-      <Button className={props.buttonClassName} onClick={handleOnClick}>
-        {props.buttonText}
-      </Button>
+      <ButtonComponent
+        isPrimary={props.isPrimaryButton}
+        onClick={handleOnClick}
+        text={props.textButton}
+        margin={props.marginButton}
+      />
       <ModalComponent isOpen={isOpen} onClose={onClose}>
-        <Box mt="91px">
+        <Box mt="90px">
           <Image style={props.style} src={props.image} alt="image" />
         </Box>
         <Box mt="60px">
-          <h1 className="md-msg">{props.titleText}</h1>
+          <TitleModal>{props.titleText}</TitleModal>
         </Box>
-        <Container mt="24px" width="507px" height="44px" alignItems="center">
-          <h1 className="md-txt">{props.contentText}</h1>
-        </Container>
+        <ContentModal>{props.contentText}</ContentModal>
       </ModalComponent>
     </>
   );
