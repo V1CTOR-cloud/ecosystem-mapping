771e294d889aa59d0ce343775e054398295fd4c4
mickael <mickael@soloso.fr>
2022-03-30 09:49:52 +0300

    [ButtonOpenModal] renaming the component according to the good practices, name a component according to his function not the purpose.

--- src/components/service/ServiceConfirmation/ButtonOpenModal.jsx
+++ src/components/service/ServiceConfirmation/ButtonOpenModal.jsx
@@ -1,31 +1,9 @@
 import React from "react";
 
-import { Box, Image, useDisclosure } from "@chakra-ui/react";
-import styled from "styled-components";
+import { useDisclosure } from "@chakra-ui/react";
 
 import ModalComponent from "../../basic/ModalComponent";
 import ButtonComponent from "../../basic/Button";
-
-const TitleModal = styled.h1`
-  font-family: Ubuntu, sans-serif;
-  font-size: 32px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 45px;
-  letter-spacing: 0.02em;
-  text-align: center;
-  color: #222c2f;
-`;
-
-const ContentModal = styled.h1`
-  font-family: Ubuntu, sans-serif;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 22px;
-  text-align: center;
-  color: #222c2f;
-`;
 
 // Component that display a button, if clicked, it will show a modal with a title, content and image.
 // Used mainly for: publishing a service; saving in draft a service.
@@ -48,13 +26,7 @@ const ButtonOpenModal = (props) => {
         margin={props.marginButton}
       />
       <ModalComponent isOpen={isOpen} onClose={onClose}>
-        <Box mt="90px">
-          <Image style={props.style} src={props.image} alt="image" />
-        </Box>
-        <Box mt="60px">
-          <TitleModal>{props.titleText}</TitleModal>
-        </Box>
-        <ContentModal>{props.contentText}</ContentModal>
+        {props.children}
       </ModalComponent>
     </>
   );
