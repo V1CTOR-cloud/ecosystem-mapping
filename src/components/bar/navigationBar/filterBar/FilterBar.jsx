20fe54264a79d63678991ed60ab64825f61b7809
mickael <mickael@soloso.fr>
2022-04-27 11:02:59 +0300

    [FilterBar] Creation of the filterBar component

---
+++ src/components/bar/navigationBar/filterBar/FilterBar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { HStack, Spacer, Text } from "@chakra-ui/react";
+import { Save } from "@styled-icons/boxicons-regular";
+
+import {
+  defaultPadding,
+  greyColor,
+  smallPadding,
+} from "../../../../helper/constant";
+import MyIconButton from "../../../basic/MyIconButton";
+import FilterMenuButton from "./FilterMenuButton";
+
+function FilterBar(props) {
+  return (
+    <HStack paddingY={smallPadding} paddingX={defaultPadding} w="100%" h="60px">
+      <Text>Filter by:</Text>
+      {props.filters.map((filter) => (
+        <FilterMenuButton
+          key={filter.name}
+          text={filter.name}
+          items={filter.items}
+          isButtonActive={props.isButtonActive}
+          handleAllClick={props.handleAllClick}
+          handleNoneClick={props.handleNoneClick}
+          handleItemClick={props.handleItemClick}
+          filter={filter}
+        />
+      ))}
+      <Spacer />
+      <MyIconButton
+        icon={<Save color={greyColor} w="15px" h="15px" />}
+        onClick={props.handleSave}
+      />
+    </HStack>
+  );
+}
+
+export default FilterBar;
