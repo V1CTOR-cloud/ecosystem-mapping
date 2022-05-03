b5905ac6b579f91217773482e3d87ee9f85beafb
mickael <mickael@soloso.fr>
2022-05-03 13:17:45 +0300

    [Row] Add the number in the canvas but need to be reviewed for the dynamic size.

--- src/components/mapCanvas/drap&dropComponent/row/Row.jsx
+++ src/components/mapCanvas/drap&dropComponent/row/Row.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 import { Droppable } from "react-beautiful-dnd";
 import styled from "styled-components";
+import { Text, Center, Box } from "@chakra-ui/react";
 
 import {
   greyHoverColor,
+  market,
+  market_and_organization,
   mediumPadding,
+  organization,
   smallPadding,
 } from "../../../../helper/constant";
 import ServiceContainer from "../service/ServiceContainer";
@@ -16,13 +20,18 @@ const RowContainer = styled.div`
   transition: background-color 0.1s ease-in-out;
   flex-grow: 1;
   min-height: 180px;
-  margin-bottom: ${(props) =>
-    props.id === "Organisation" ? 0 : mediumPadding};
-  margin-top: ${(props) => (props.id === "Market" ? smallPadding : 0)};
+  margin-bottom: ${(props) => (props.id === organization ? 0 : mediumPadding)};
+  margin-top: ${(props) => (props.id === market ? smallPadding : 0)};
   position: relative;
 `;
 
+const NumberContainer = styled.div`
+  width: calc(100% / 6);
+`;
+
 function Row(props) {
+  const numbers = [-2, -1, 0, 1, 2, 3];
+
   return (
     <Droppable droppableId={props.row.id}>
       {(
@@ -41,27 +50,41 @@ function Row(props) {
                 key={service.id}
                 service={service}
                 index={index}
+                handleServiceClick={props.handleServiceClick}
               />
             ))}
             {provided.placeholder}
-            {/*TODO faire les numéro: trouver une solution pour les centrer automatiquement au milieu*/}
-            {/*{props.row.id === "market_organization" && (*/}
-            {/*  <Box*/}
-            {/*    zIndex="100000"*/}
-            {/*    bg="#555555"*/}
-            {/*    w="100%"*/}
-            {/*    position="absolute"*/}
-            {/*    display="flex"*/}
-            {/*    justifyContent="space-between"*/}
-            {/*  >*/}
-            {/*    <Box>-2</Box>*/}
-            {/*    <Box>-1</Box>*/}
-            {/*    <Box>0</Box>*/}
-            {/*    <Box>1</Box>*/}
-            {/*    <Box>2</Box>*/}
-            {/*    <Box>3</Box>*/}
-            {/*  </Box>*/}
-            {/*)}*/}
+            {props.row.id === market_and_organization && (
+              <Box
+                zIndex="0"
+                w="100%"
+                position="absolute"
+                display="flex"
+                justifyContent="space-between"
+                align="center"
+                //TODO change for the dynamically size
+                top="50px"
+              >
+                {numbers.map((number) => {
+                  return (
+                    <NumberContainer key={number}>
+                      <Box
+                        borderRadius="50%"
+                        border={`2px solid ${greyHoverColor}`}
+                        w="70px"
+                        h="70px"
+                      >
+                        <Center h="100%" w="100%">
+                          <Text fontSize="30px" textColor={greyHoverColor}>
+                            {number}
+                          </Text>
+                        </Center>
+                      </Box>
+                    </NumberContainer>
+                  );
+                })}
+              </Box>
+            )}
           </RowContainer>
         );
       }}
