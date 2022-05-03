be87a12f71fa96eea7459439d84a2b121d2554e4
mickael <mickael@soloso.fr>
2022-05-03 13:22:35 +0300

    [ToastComponent] Creation of this component to alert the user when a modification/creation/delete was taking into account or that we had an error.

--- src/components/basic/ToastComponent.jsx
+++ src/components/basic/ToastComponent.jsx
@@ -1,14 +1,14 @@
 import { createStandaloneToast } from "@chakra-ui/react";
 
 // Display a toast with the text pass props. For the status: "success", "error", "warning", "info" are available.
-function toastComponent(text, status) {
+function toastComponent(text, status, duration) {
   const toast = createStandaloneToast();
 
   return toast({
     title: text,
     status: status,
     isClosable: true,
-    duration: 2000,
+    duration: duration ? duration : 2000,
   });
 }
 
