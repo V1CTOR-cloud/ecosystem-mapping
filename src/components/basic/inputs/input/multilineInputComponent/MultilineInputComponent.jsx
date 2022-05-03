beabc5a311f5af52813e2997aee1e20899e35a8c
mickael <mickael@soloso.fr>
2022-05-03 13:15:54 +0300

    [MultilineComponent] Creation of the Multiline component, the user resize the input

---
+++ src/components/basic/inputs/input/multilineInputComponent/MultilineInputComponent.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { Textarea } from "@chakra-ui/react";
+
+import { blueColor, greyColor } from "../../../../../helper/constant";
+
+function MultilineInputComponent(props) {
+  return (
+    <Textarea
+      isRequired={props.isRequired}
+      value={props.value}
+      onChange={props.handleOnChange}
+      placeholder={props.placeholder}
+      size="md"
+      border={`2px solid`}
+      focusBorderColor={blueColor}
+      borderColor={greyColor}
+    />
+  );
+}
+
+export default React.memo(MultilineInputComponent);
