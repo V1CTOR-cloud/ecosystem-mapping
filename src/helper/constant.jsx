5a07113736ba3d7d24856debcd488f16f570cf92
mickael <mickael@soloso.fr>
2022-05-10 11:31:28 +0300

    [constant] Remove unused constants that are no longer used in the projet.

--- src/helper/constant.jsx
+++ src/helper/constant.jsx
@@ -1,13 +1,3 @@
-import publishImage from "../assets/images/service published graphic (1).png";
-import draftImage from "../assets/images/Save time and effort 1 (1).png";
-import styled from "styled-components";
-
-export const financeProfessional = require("assets/images/In-HouseFinanceProfessional12.png");
-export const SliderButton = require("assets/images/Ellipse.png");
-
-export const constantPublishImage = publishImage;
-export const constantDraftImage = draftImage;
-
 // COLORS
 export const blueColor = "#074EE8";
 export const blueHoverColor = "#4D84F9";
@@ -36,44 +26,7 @@ export const verySmallPadding = "8px";
 export const borderRadius = "4px";
 export const borderThickness = "2px";
 
-export const constantPublishImageStyle = {
-  height: "auto",
-  width: "383.09px",
-  margin: "0 auto",
-  top: "55.86px",
-  align: "center",
-};
-
-export const constantDraftImageStyle = {
-  height: "auto",
-  width: "399.4375915527344px",
-  margin: "0 auto",
-  top: "55.108154296875px",
-  borderRadius: "0px",
-  align: "center",
-};
-
-export const TitleModal = styled.h1`
-  font-family: Ubuntu, sans-serif;
-  font-size: 32px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 45px;
-  letter-spacing: 0.02em;
-  text-align: center;
-  color: #222c2f;
-`;
-
-export const ContentModal = styled.h1`
-  font-family: Ubuntu, sans-serif;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 22px;
-  text-align: center;
-  color: #222c2f;
-`;
-
+// STRINGS
 export const market_and_organization = "Market_and_Organization";
 export const market = "Market";
 export const organization = "Organization";
