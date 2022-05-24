f76ef8f163e2afc5293f46742d0c078f4b600026
mickael <mickael@soloso.fr>
2022-05-24 12:06:41 +0300

    [FilterBar] We seperate now the saved filter from the others because he has too many things different. (He can control the others) Creation of the function that set all others filters corresponding to the filter saved previously.

--- src/components/bar/navigationBar/filterBar/FilterBar.jsx
+++ src/components/bar/navigationBar/filterBar/FilterBar.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { HStack, Spacer, Text, Box, useDisclosure } from "@chakra-ui/react";
 import { Save } from "@styled-icons/boxicons-regular";
+import { Check2Circle } from "@styled-icons/bootstrap";
 import { useTranslation } from "react-i18next";
 
 import {
@@ -10,27 +11,78 @@ import {
   greyTextColor,
   smallPadding,
   verySmallPadding,
+  whiteColor,
 } from "../../../../helper/constant";
-import FilterMenuButton from "./FilterMenuButton";
+import FilterMenuButton from "./filtersButtons/FilterMenuButton";
 import ButtonComponent from "../../../basic/Buttons/ButtonComponent";
 import SaveFilterAlertDialog from "./SaveFilterAlertDialog";
+import SavedFilterButton from "./filtersButtons/SavedFilterButton";
 
 function FilterBar(props) {
   const { t } = useTranslation();
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [filters, setFilters] = useState(props.filtersState[0]);
+  const [isActive, setIsActive] = useState(
+    filters.some((filter) => filter.selectedFilterCount > 0)
+  );
+  const [isFilterApplied, setIsFilterApplied] = useState(false);
+  const [value, setValue] = useState("");
+  const [isSavedFilterSelected, setIsSavedFilterSelected] = useState(false);
 
-  const isActive = filters.some((filter) => filter.selectedFilterCount > 0);
+  useEffect(() => {
+    setIsSavedFilterSelected(value !== "");
+  }, [value]);
+
+  useEffect(() => {
+    if (filters.some((filter) => filter.selectedFilterCount > 0)) {
+      setIsActive(true);
+      setIsFilterApplied(false);
+    }
+  }, [filters]);
 
   function handleFilterChange(filter, index) {
     const tempFilters = [...filters];
     tempFilters[index] = filter;
+    setValue("");
     setFilters(tempFilters);
   }
 
   function handleApplyFilter() {
     // Update in the canvas the display of the service every filter changes
     props.filtersState[1](filters);
+    setIsFilterApplied(true);
+  }
+
+  function handleClearAllFilters() {
+    props.handleClearAllFilters();
+    setIsActive(false);
+  }
+
+  function handleSavedFilterChange(filter) {
+    const savedFiltersEntries = Object.entries(filter[1]);
+
+    const tempFilters = [...filters];
+
+    // Go through all the filter saved
+    savedFiltersEntries.forEach((thisFilter) => {
+      // Get the value of each element in a specific filter
+      thisFilter[1].items.forEach((item) => {
+        const index = tempFilters[thisFilter[0]].items.findIndex(
+          (thisItem) => thisItem.name === item
+        );
+
+        // Set the value to true from the specific filter
+        tempFilters[thisFilter[0]].items[index].value = true;
+      });
+
+      // Set the selectedFilterCount
+      tempFilters[thisFilter[0]].selectedFilterCount =
+        thisFilter[1].items.length;
+    });
+
+    setFilters(tempFilters);
+
+    setValue(filter[0]);
   }
 
   return (
@@ -38,13 +90,39 @@ function FilterBar(props) {
       <Text color={greyTextColor}>
         {t("mapping.navigation.filter.bar.filter.by")}
       </Text>
-      {props.filtersState[0].map((filter, index) => (
-        <FilterMenuButton
-          key={filter.name}
-          filter={filter}
-          handleFilterChange={(filter) => handleFilterChange(filter, index)}
+      {props.filtersState[0].map((filter, index) => {
+        if (index === 0) {
+          return (
+            <SavedFilterButton
+              key={filter.name}
+              filter={filter}
+              savedFilters={props.savedFilters}
+              handleSavedFilterChange={(filter) =>
+                handleSavedFilterChange(filter)
+              }
+              value={value}
+            />
+          );
+        } else {
+          return (
+            <FilterMenuButton
+              key={filter.name}
+              filter={filter}
+              savedFilters={props.savedFilters}
+              handleFilterChange={(filter) => handleFilterChange(filter, index)}
+            />
+          );
+        }
+      })}
+      {isActive && !isFilterApplied && (
+        <ButtonComponent
+          buttonText={"Apply Filters"}
+          isWithoutBorder={true}
+          isSelected={true}
+          icon={<Check2Circle color={blueColor} size={25} />}
+          onClick={handleApplyFilter}
         />
-      ))}
+      )}
       <Spacer />
 
       {isActive && (
@@ -53,29 +131,29 @@ function FilterBar(props) {
             as="u"
             cursor="pointer"
             color={greyTextColor}
-            onClick={props.handleClearAllFilters}
+            onClick={handleClearAllFilters}
           >
             {t("mapping.navigation.bar.clear.filter.button")}
           </Text>
         </Box>
       )}
-
       {isActive && (
         <Box paddingRight={verySmallPadding}>
           <ButtonComponent
-            buttonText={"Save Filter"}
-            isPrimary={false}
-            icon={<Save color={blueColor} size={25} />}
-            onClick={onOpen}
+            buttonText={isSavedFilterSelected ? "Saved Filter" : "Save Filter"}
+            isPrimary={!isSavedFilterSelected}
+            isWithoutBorder={isSavedFilterSelected}
+            isSelected={isSavedFilterSelected}
+            icon={
+              isSavedFilterSelected ? (
+                <Check2Circle color={blueColor} size={25} />
+              ) : (
+                <Save color={whiteColor} size={25} />
+              )
+            }
+            onClick={isSavedFilterSelected ? () => {} : onOpen}
           />
         </Box>
-      )}
-      {isActive && (
-        <ButtonComponent
-          buttonText={"Apply Filters"}
-          isPrimary={true}
-          onClick={handleApplyFilter}
-        />
       )}
       <SaveFilterAlertDialog
         isOpen={isOpen}
@@ -83,6 +161,7 @@ function FilterBar(props) {
         setFilters={props.filtersState[1]}
         filters={filters}
         mapId={props.mapId}
+        savedFilters={props.savedFilters}
       />
     </HStack>
   );
