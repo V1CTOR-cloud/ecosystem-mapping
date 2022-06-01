3ac9bfa67926d760085a59c60fe89a28c65cc395
mickael <mickael@soloso.fr>
2022-06-01 10:17:28 +0300

    [constant] Add the list of audience because she will not change.

--- src/helper/constant.jsx
+++ src/helper/constant.jsx
@@ -32,3 +32,11 @@ export const borderThickness = "2px";
 export const market_and_organization = "Market_and_Organization";
 export const market = "Market";
 export const organization = "Organization";
+
+// LISTS
+export const audienceList = [
+  { id: 1, name: "Support Service Staff" },
+  { id: 2, name: "Talent Joining Startups" },
+  { id: 3, name: "Co Founders" },
+  { id: 4, name: "New Founders" },
+];
