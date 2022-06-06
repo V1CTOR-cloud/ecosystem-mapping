2cbf873e41c18a3830ea14f1e36f272b37fc8b7b
mickael <mickael@soloso.fr>
2022-06-06 12:00:16 +0300

    [organisation.js] add function "addOrganisation".

--- src/service/organisation.js
+++ src/service/organisation.js
@@ -12,5 +12,23 @@ export const Organisation = {
     }`;
 
     return await graphCMSRequest(query);
+  },
+
+  async addOrganisation(data) {
+    const query = `mutation ($data: OrganisationCreateInput!) {
+          createOrganisation(data: $data){id}
+      }`;
+
+    const variables = {
+      data: {
+        organisationName: data.orgName,
+        startingStageRange: parseInt(data.startRange),
+        endingStageRange: parseInt(data.endRange),
+        organizationType: data.type,
+        websiteURL: data.orgUrl
+      }
+    };
+
+    return await graphCMSRequest(query, variables);;
   }
-}
+};
