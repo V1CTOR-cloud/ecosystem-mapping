6d30d162ccc9024f0fa7fad009a4bde8eecfae5e
mickael <mickael@soloso.fr>
2022-06-07 13:28:52 +0300

    [MultilineInputComponent] Add propTypes with isRequired

--- src/components/basic/inputs/input/multilineInputComponent/MultilineInputComponent.jsx
+++ src/components/basic/inputs/input/multilineInputComponent/MultilineInputComponent.jsx
@@ -3,21 +3,23 @@ import React, { memo, useState } from "react";
 import { Textarea } from "@chakra-ui/react";
 
 import { blueColor, greyColor } from "../../../../../helper/constant";
+import PropTypes from "prop-types";
 
 function MultilineInputComponent(props) {
-  const [value, setValue] = useState(props.value);
+  const { propValue, onChange, isRequired, placeholder } = props;
+  const [value, setValue] = useState(propValue);
 
   function handleOnChange(event) {
     setValue(event.target.value);
-    props.onChange(event.target.value);
+    onChange(event.target.value);
   }
 
   return (
     <Textarea
-      isRequired={props.isRequired}
+      isRequired={isRequired}
       value={value}
       onChange={handleOnChange}
-      placeholder={props.placeholder}
+      placeholder={placeholder}
       size="md"
       border={`2px solid`}
       focusBorderColor={blueColor}
@@ -25,5 +27,12 @@ function MultilineInputComponent(props) {
     />
   );
 }
+
+MultilineInputComponent.propTypes = {
+  isRequired: PropTypes.bool.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  propValue: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
 
 export default memo(MultilineInputComponent);
