fab4159e842ef2d80c9b43c0a66eeeecd3b993eb
mickael <mickael@soloso.fr>
2022-06-07 13:43:53 +0300

    [MenuComponent] Add propTypes with isRequired

--- src/components/basic/inputs/menu/MenuComponent.jsx
+++ src/components/basic/inputs/menu/MenuComponent.jsx
@@ -1,47 +1,68 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
-import {Button, MenuButton, MenuItem, MenuList, Menu, Box} from "@chakra-ui/react";
-import {ChevronDownIcon} from "@chakra-ui/icons";
+import {
+  Button,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Menu,
+  Box,
+} from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import PropTypes from "prop-types";
 
 function MenuComponent(props) {
-    const [value, setValue] = useState(props.item);
+  const { item, onChange, width, isDisabled, wantScroll, items } = props;
+  const [value, setValue] = useState(item);
 
-    useEffect(() => {
-        setValue(props.item);
-    }, [props.item]);
+  useEffect(() => {
+    setValue(item);
+  }, [item]);
 
-    function handleOnChange(event) {
-        setValue(event.target.lastChild.data);
-        props.onChange(event.target.lastChild.data);
-    }
+  function handleOnChange(event) {
+    setValue(event.target.lastChild.data);
+    onChange(event.target.lastChild.data);
+  }
 
-    return (
-        <Box>
-            <Menu isLazy>
-                <MenuButton
-                    w={props.width ? props.width : undefined}
-                    as={Button}
-                    rightIcon={<ChevronDownIcon/>}
-                    isDisabled={props.isDisabled ? props.isDisabled : false}
-                >
-                    {value}
-                </MenuButton>
-                <MenuList h={props.wantScroll ? "300px" : "auto"} overflowY="scroll">
-                    {props.items.map((item) => {
-                        return (
-                            <MenuItem key={item.id} onClick={(event) => handleOnChange(event)}>
-                                {item.name}
-                            </MenuItem>
-                        );
-                    })}
-                </MenuList>
-            </Menu>
-        </Box>
-    );
+  return (
+    <Box>
+      <Menu isLazy>
+        <MenuButton
+          w={width ? width : undefined}
+          as={Button}
+          rightIcon={<ChevronDownIcon />}
+          isDisabled={isDisabled ? isDisabled : false}
+        >
+          {value}
+        </MenuButton>
+        <MenuList h={wantScroll ? "300px" : "auto"} overflowY="scroll">
+          {items.map((item) => {
+            return (
+              <MenuItem
+                key={item.id}
+                onClick={(event) => handleOnChange(event)}
+              >
+                {item.name}
+              </MenuItem>
+            );
+          })}
+        </MenuList>
+      </Menu>
+    </Box>
+  );
 }
 
 MenuComponent.defaultProps = {
-    wantScroll: false,
+  wantScroll: false,
 };
 
-export default React.memo(MenuComponent);
+MenuComponent.propTypes = {
+  wantScroll: PropTypes.bool,
+  isDisabled: PropTypes.bool.isRequired,
+  width: PropTypes.string.isRequired,
+  item: PropTypes.object.isRequired,
+  items: PropTypes.array.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default MenuComponent;
