a8b7032a5edcfed5ba52bae49919ecd18da02ec3
mickael <mickael@soloso.fr>
2022-06-07 13:39:01 +0300

    [LabeledMenu] Add propTypes with isRequired

--- src/components/basic/inputs/menu/LabeledMenu.jsx
+++ src/components/basic/inputs/menu/LabeledMenu.jsx
@@ -4,22 +4,29 @@ import { Box } from "@chakra-ui/react";
 
 import LabelWithTooltip from "../../labelWithTooltip/LabelWithTooltip";
 import MenuComponent from "./MenuComponent";
+import PropTypes from "prop-types";
 
 function LabeledMenu(props) {
+  const { tooltipText, tooltipAriaLabel, label, item, items, onChange } = props;
   return (
     <Box>
       <LabelWithTooltip
-        tooltipText={props.tooltipText}
-        tooltipAriaLabel={props.tooltipAriaLabel}
-        label={props.label}
+        tooltipText={tooltipText}
+        tooltipAriaLabel={tooltipAriaLabel}
+        label={label}
       />
-      <MenuComponent
-        item={props.item}
-        items={props.items}
-        onChange={props.onChange}
-      />
+      <MenuComponent item={item} items={items} onChange={onChange} />
     </Box>
   );
 }
 
-export default React.memo(LabeledMenu);
+LabeledMenu.propTypes = {
+  tooltipText: PropTypes.string.isRequired,
+  tooltipAriaLabel: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  item: PropTypes.object.isRequired,
+  items: PropTypes.array.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default LabeledMenu;
