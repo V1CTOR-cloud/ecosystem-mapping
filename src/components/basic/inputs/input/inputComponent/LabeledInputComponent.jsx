856e819cfa200f33b7cdeeddce7d0f501da15195
mickael <mickael@soloso.fr>
2022-06-07 13:21:22 +0300

    [LabeledInputComponent] Add propTypes with isRequired

--- src/components/basic/inputs/input/inputComponent/LabeledInputComponent.jsx
+++ src/components/basic/inputs/input/inputComponent/LabeledInputComponent.jsx
@@ -1,26 +1,47 @@
 import React from "react";
 
 import { Box } from "@chakra-ui/react";
+import PropTypes from "prop-types";
 
 import LabelWithTooltip from "../../../labelWithTooltip/LabelWithTooltip";
 import InputComponent from "./InputComponent";
 
 function LabeledInputComponent(props) {
+  const {
+    tooltipText,
+    tooltipAriaLabel,
+    label,
+    value,
+    placeholder,
+    handleOnChange,
+    onChange,
+  } = props;
+
   return (
     <Box>
       <LabelWithTooltip
-        tooltipText={props.tooltipText}
-        tooltipAriaLabel={props.tooltipAriaLabel}
-        label={props.label}
+        tooltipText={tooltipText}
+        tooltipAriaLabel={tooltipAriaLabel}
+        label={label}
       />
       <InputComponent
-        value={props.value}
-        placeholder={props.placeholder}
-        handleOnChange={props.handleOnChange}
-        onChange={props.onChange}
+        value={value}
+        placeholder={placeholder}
+        handleOnChange={handleOnChange}
+        onChange={onChange}
       />
     </Box>
   );
 }
+
+LabeledInputComponent.propTypes = {
+  tooltipText: PropTypes.string.isRequired,
+  tooltipAriaLabel: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  handleOnChange: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
 
 export default React.memo(LabeledInputComponent);
