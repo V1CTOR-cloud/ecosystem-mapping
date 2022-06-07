3116767bf5b62bccb0063bab68465d367f42eeb6
mickael <mickael@soloso.fr>
2022-06-07 10:47:07 +0300

    [App.js] Modification in the structure of the file. Now we have a separate function that handle the navigation. We have a gain in performance.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React, { useEffect, useState } from "react";
 
 import { Box, ChakraProvider, Grid, theme } from "@chakra-ui/react";
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch,
-} from "react-router-dom";
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import axios from "axios";
 
 import "./assets/css/Style.scss";
-import LandingPage from "./pages/LandingPage";
-import ListMapPage from "./pages/ListMapPage";
-import MapCanvasPage from "./pages/MapCanvasPage";
-import { Authentication } from "./service/authentication";
+import Routing from "./Routing";
+
 
 function App() {
-  const [resource, setresource] = useState("");
-  const [fetched, setFetched] = useState(false);
+  const [resource, setResource] = useState("");
 
   useEffect(() => {
     axios
@@ -27,74 +18,35 @@ function App() {
         "https://jqdryvpiqf.execute-api.us-east-2.amazonaws.com/default/i18reader"
       )
       .then((response) => {
-        setresource(response.data);
-        setFetched(true);
+        setResource(response.data);
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
 
-  if (fetched) {
-    i18n.use(initReactI18next).init({
-      resources: {
-        en: {
-          translation: {
-            ...resource.translation,
-          },
-        },
-      },
-      lng: "en",
-      interpolation: {
-        escapeValue: false,
-      },
-    });
-  }
+  i18n.use(initReactI18next).init({
+    resources: {
+      en: {
+        translation: {
+          ...resource.translation
+        }
+      }
+    },
+    lng: "en",
+    interpolation: {
+      escapeValue: false
+    }
+  });
 
-  return fetched ? (
+  return (
     <ChakraProvider theme={theme}>
       <Box fontSize="xl">
         <Grid minH="100vh">
-          <Router>
-            <Switch>
-              <Route exact path="/" component={LandingPage} />
-              <Route
-                exact
-                path="/home"
-                render={() =>
-                  Authentication.isLoggedIn() ? <MapCanvasPage /> : <Redirect to="/" />
-                }
-              />
-              <Route
-                exact
-                path="/ecosystemmap"
-                render={() =>
-                  Authentication.isLoggedIn() ? <ListMapPage /> : <Redirect to="/" />
-                }
-              />
-              <Route
-                exact
-                path="/services/:serviceId"
-                render={() => {
-                  const pathName = window.location.pathname.toString();
-                  const pathNameSplit = pathName.split("/");
-                  const mapId = pathNameSplit[2];
-
-                  return Authentication.isLoggedIn() ? (
-                    <MapCanvasPage mapId={mapId} />
-                  ) : (
-                    <Redirect to="/" />
-                  );
-                }}
-              />
-              <Route render={() => <LandingPage />} />
-            </Switch>
-          </Router>
+          {resource === "" ? <div>Loading...</div> : <Routing />}
         </Grid>
       </Box>
     </ChakraProvider>
-  ) : (
-    <Box>Loading...</Box>
   );
 }
 
