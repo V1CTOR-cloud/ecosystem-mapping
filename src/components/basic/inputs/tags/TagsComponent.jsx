1888c520fe08d7513eca9b0c6c357db0194b0907
mickael <mickael@soloso.fr>
2022-06-08 11:14:53 +0300

    [TagsComponents] Add proptypes to all files.

--- src/components/basic/inputs/tags/TagsComponent.jsx
+++ src/components/basic/inputs/tags/TagsComponent.jsx
@@ -2,16 +2,18 @@ import React from "react";
 
 import TagsInput from "react-tagsinput";
 import tagsComponentStyle from "./tagsComponent.css";
+import PropTypes from "prop-types";
 
 function TagComponent(props) {
+  const { handleTagsChange, tags } = props;
   function handleTagsChange(tag) {
-    props.handleTagsChange(tag);
+    handleTagsChange(tag);
   }
 
   return (
     <div className={tagsComponentStyle}>
       <TagsInput
-        value={props.tags}
+        value={tags}
         onChange={(tag) => handleTagsChange(tag)}
         onlyUnique
         addOnPaste
@@ -19,5 +21,10 @@ function TagComponent(props) {
     </div>
   );
 }
+
+TagComponent.propTypes = {
+  tags: PropTypes.array.isRequired,
+  handleTagsChange: PropTypes.func.isRequired,
+};
 
 export default TagComponent;
