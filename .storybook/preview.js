8e9f1e974534544ebb78b9277167b285fbca842c
mickael <mickael@soloso.fr>
2022-06-13 15:12:07 +0300

    Add storybook to the depedencies

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,12 @@
-import {
-  ChakraProvider,
-  theme,
-} from "@chakra-ui/react"
-import * as React from "react"
+import { ChakraProvider } from "@chakra-ui/react";
+import * as React from "react";
 
-
+import { theme } from "../src/theme/theme";
 
 export const withChakra = () => {
-
   return (
     <ChakraProvider theme={theme}>
-        <StoryFn />
+      <StoryFn />
     </ChakraProvider>
-  )
-}
-
+  );
+};
