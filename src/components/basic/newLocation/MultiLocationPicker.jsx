3cf7b07d064c151797e976a6b3ba6f7a5771b10b
mickael <mickael@soloso.fr>
2022-06-21 14:57:37 +0300

    [MultiLocationPicker + multiLocation.stories.mdx] Creation of the component that allow to pick multiple values of locations. The creation (without initials values) and edition mode are working. Documentation done alongside the creation of the component.

---
+++ src/components/basic/newLocation/MultiLocationPicker.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+
+import { Box, Link } from "@chakra-ui/react";
+import PropTypes from "prop-types";
+
+import LabelWithTooltip from "../labelWithTooltip/LabelWithTooltip";
+import NewLocation from "./NewLocation";
+
+function MultiLocationPicker(props) {
+  const { initialValues, onChange, locationsList } = props;
+  const [locations, setLocations] = useState(initialValues);
+  console.log(locationsList);
+
+  // Update the formValues everytime we change a field of a location.
+  useEffect(() => {
+    onChange(locations);
+  }, [locations, onChange]);
+
+  // Add industry at the last index of the array.
+  function handleAddLocation() {
+    setLocations((previousState) => {
+      return [
+        ...previousState,
+        {
+          continent: null,
+          country: null,
+          region: null,
+          city: null,
+        },
+      ];
+    });
+  }
+
+  // Remove the industry selected.
+  function handleRemoveLocation(index) {
+    const tempLocations = [...locations];
+    tempLocations.splice(index, 1);
+    setLocations(tempLocations);
+  }
+
+  return (
+    <>
+      <LabelWithTooltip
+        label={"Location"}
+        tooltipText={"Tooltip location"}
+        tooltipAriaLabel={"Location"}
+      />
+      {locations.map((location, index) => {
+        return (
+          <Box key={index} marginY={2}>
+            <NewLocation
+              locationsState={[locations, setLocations]}
+              index={index}
+              items={locationsList}
+              isFirst={index === 0}
+              onRemoveLocation={() => handleRemoveLocation(index)}
+            />
+          </Box>
+        );
+      })}
+      <Link color="brand.500" fontSize="xs" onClick={handleAddLocation}>
+        + Add another location
+      </Link>
+    </>
+  );
+}
+
+export default MultiLocationPicker;
+
+MultiLocationPicker.propTypes = {
+  /**
+   * Array of object that contains all the initial locations.
+   */
+  initialValues: PropTypes.array.isRequired,
+  /**
+   * Array of object that contains all the locations, it comes from either the appProvider or the story loader.
+   */
+  locationsList: PropTypes.array.isRequired,
+  /**
+   * Function that will update the value of the model with a setState for example.
+   */
+  onChange: PropTypes.func.isRequired,
+};
