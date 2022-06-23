6675c0fa089bf74031335f4d34363dbb4ca19701
mickael <mickael@soloso.fr>
2022-06-23 11:11:29 +0300

    [DashboardMenuOptions] Passing to the menu the data of the map that we have currently selected + call the archive function when needed.

--- src/components/dashboard/mapView/DashboardMenuOptions.jsx
+++ src/components/dashboard/mapView/DashboardMenuOptions.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import {
   Box,
@@ -14,9 +14,13 @@ import {
 import { CopyIcon, EditIcon } from "@chakra-ui/icons";
 import { Share, ThreeDotsVertical } from "@styled-icons/bootstrap";
 import { Archive } from "@styled-icons/boxicons-regular";
+import PropTypes from "prop-types";
+import { DashboardProvider } from "../../../pages/Dashboard";
 
-function DashboardMenuOptions() {
+function DashboardMenuOptions(props) {
+  const { mapData } = props;
   const transition = "color 0.25s";
+  const dashboardProvider = useContext(DashboardProvider);
 
   const menuOptions = [
     {
@@ -24,7 +28,7 @@ function DashboardMenuOptions() {
       title: "Edit",
       description: "Change the name or other info of the map.",
       icon: <EditIcon w="25px" h="25px" />,
-      buttonFunction: () => console.log("Edit"),
+      buttonFunction: () => console.log(mapData),
     },
     {
       id: 1,
@@ -45,71 +49,81 @@ function DashboardMenuOptions() {
       title: "Archive",
       description: "Move the ecosystem map and mark it as unpublished.",
       icon: <Archive size="25px" />,
-      buttonFunction: () => console.log("Archive"),
+      buttonFunction: () => dashboardProvider.archiveFunction(mapData),
     },
   ];
 
   return (
-    <Menu>
-      <MenuButton
-        as={Button}
-        size="sm"
-        variant="greyGhost"
-        borderColor="blackAlpha.500"
-        _hover={{ bg: "blackAlpha.200" }}
-        _active={{ bg: "blackAlpha.300" }}
-      >
-        <ThreeDotsVertical size="22.5px" />
-      </MenuButton>
-      <MenuList w="300px">
-        {menuOptions.map((button, index) => {
-          return (
-            <MenuItem
-              className="menuItem"
-              key={button.id}
-              _hover={{
-                backgroundColor: index === 3 ? "red.100" : undefined,
-              }}
-              borderTopRadius={index === 0 ? "base" : undefined}
-              borderBottomRadius={index === 3 ? "base" : undefined}
-            >
-              <HStack>
-                <Box>{button.icon}</Box>
-                <VStack paddingLeft="2" spacing="0">
-                  <Text
-                    className="title"
-                    w="100%"
-                    fontSize="sm"
-                    transition={transition}
-                    sx={{
-                      ".menuItem:hover &": {
-                        color: index === 3 ? "red" : "brand.500",
-                      },
-                    }}
-                  >
-                    {button.title}
-                  </Text>
-                  <Text
-                    className="description"
-                    fontSize="xs"
-                    color="blackAlpha.500"
-                    transition={transition}
-                    sx={{
-                      ".menuItem:hover &": {
-                        color: "black",
-                      },
-                    }}
-                  >
-                    {button.description}
-                  </Text>
-                </VStack>
-              </HStack>
-            </MenuItem>
-          );
-        })}
-      </MenuList>
-    </Menu>
+    <Box>
+      <Menu>
+        <MenuButton
+          as={Button}
+          size="sm"
+          variant="greyGhost"
+          borderColor="blackAlpha.500"
+          _hover={{ bg: "blackAlpha.200" }}
+          _active={{ bg: "blackAlpha.300" }}
+        >
+          <ThreeDotsVertical size="22.5px" />
+        </MenuButton>
+        <MenuList w="300px">
+          {menuOptions.map((button, index) => {
+            return (
+              <MenuItem
+                className="menuItem"
+                key={button.id}
+                _hover={{
+                  backgroundColor: index === 3 ? "red.100" : undefined,
+                }}
+                borderTopRadius={index === 0 ? "base" : undefined}
+                borderBottomRadius={index === 3 ? "base" : undefined}
+                onClick={button.buttonFunction}
+              >
+                <HStack>
+                  <Box>{button.icon}</Box>
+                  <VStack paddingLeft={2} spacing="0">
+                    <Text
+                      className="title"
+                      w="100%"
+                      fontSize="sm"
+                      transition={transition}
+                      sx={{
+                        ".menuItem:hover &": {
+                          color: index === 3 ? "red" : "brand.500",
+                        },
+                      }}
+                    >
+                      {button.title}
+                    </Text>
+                    <Text
+                      className="description"
+                      fontSize="xs"
+                      color="blackAlpha.500"
+                      transition={transition}
+                      sx={{
+                        ".menuItem:hover &": {
+                          color: "black",
+                        },
+                      }}
+                    >
+                      {button.description}
+                    </Text>
+                  </VStack>
+                </HStack>
+              </MenuItem>
+            );
+          })}
+        </MenuList>
+      </Menu>
+    </Box>
   );
 }
 
 export default DashboardMenuOptions;
+
+DashboardMenuOptions.propTypes = {
+  /**
+   * Object that contains all the information of the map clicked. Useful for duplicating, edition and putting in archive.
+   */
+  mapData: PropTypes.object.isRequired,
+};
