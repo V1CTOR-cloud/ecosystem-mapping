727f1aef2951c2338b237d588b5553d15a711639
mickael <mickael@soloso.fr>
2022-06-27 13:07:20 +0300

    Add the redirection to the canvas once clicked on the dashboard map.

--- src/components/dashboard/mapView/ListMap.jsx
+++ src/components/dashboard/mapView/ListMap.jsx
@@ -5,6 +5,7 @@ import PropTypes from "prop-types";
 import moment from "moment";
 
 import DashboardMenuOptions from "./DashboardMenuOptions";
+import { NavLink } from "react-router-dom";
 
 function ListMap(props) {
   const { data } = props;
@@ -19,109 +20,111 @@ function ListMap(props) {
 
   return (
     <Box role="group">
-      <Box
-        w="100%"
-        h="150px"
-        borderBottom="3px solid"
-        borderColor={"blackAlpha.500"}
-        transition="border-color 0.25s"
-        padding={4}
-        _groupHover={{ borderColor: "brand.500" }}
-      >
-        <HStack h="100%">
-          <Image
-            h="100px"
-            w="200px"
-            fit="cover"
-            border="2px solid"
-            borderRadius="base"
-            borderColor="blackAlpha.500"
-            src={data.image}
-          />
-          <HStack h="100%" w="100%" alignItems="end" paddingY={3} spacing={0}>
-            <VStack align="start" h="100%" spacing={0}>
-              <Text
-                fontSize="xl"
-                transition={transition}
-                _groupHover={{ color: "brand.500" }}
-              >
-                {data.title}
-              </Text>
+      <NavLink to={`/dashboard/${data.id}`}>
+        <Box
+          w="100%"
+          h="150px"
+          borderBottom="3px solid"
+          borderColor={"blackAlpha.500"}
+          transition="border-color 0.25s"
+          padding={4}
+          _groupHover={{ borderColor: "brand.500" }}
+        >
+          <HStack h="100%">
+            <Image
+              h="100px"
+              w="200px"
+              fit="cover"
+              border="2px solid"
+              borderRadius="base"
+              borderColor="blackAlpha.500"
+              src={data.image}
+            />
+            <HStack h="100%" w="100%" alignItems="end" paddingY={3} spacing={0}>
+              <VStack align="start" h="100%" spacing={0}>
+                <Text
+                  fontSize="xl"
+                  transition={transition}
+                  _groupHover={{ color: "brand.500" }}
+                >
+                  {data.title}
+                </Text>
+                <Spacer />
+                <Text
+                  color={greyColor}
+                  transition={transition}
+                  _groupHover={groupBlackColor}
+                >
+                  {data.location[0].continent === null
+                    ? "No location"
+                    : formatString(data.location[0].continent) +
+                      " " +
+                      formatString(data.location[0].country) +
+                      " " +
+                      formatString(data.location[0].region) +
+                      " " +
+                      formatString(data.location[0].city)}
+                </Text>
+                <Text
+                  color={greyColor}
+                  paddingRight={5}
+                  transition={transition}
+                  _groupHover={groupBlackColor}
+                >
+                  {data.industry[0].mainIndustry === null
+                    ? "No industry"
+                    : formatString(data.industry[0].mainIndustry) +
+                      " " +
+                      formatString(data.industry[0].subIndustry)}
+                </Text>
+              </VStack>
               <Spacer />
               <Text
                 color={greyColor}
                 transition={transition}
                 _groupHover={groupBlackColor}
               >
-                {data.location[0].continent === null
-                  ? "No location"
-                  : formatString(data.location[0].continent) +
-                    " " +
-                    formatString(data.location[0].country) +
-                    " " +
-                    formatString(data.location[0].region) +
-                    " " +
-                    formatString(data.location[0].city)}
+                Owner: {data.owner.profileName}
               </Text>
               <Text
                 color={greyColor}
-                paddingRight={5}
                 transition={transition}
                 _groupHover={groupBlackColor}
+                paddingX={5}
               >
-                {data.industry[0].mainIndustry === null
-                  ? "No industry"
-                  : formatString(data.industry[0].mainIndustry) +
-                    " " +
-                    formatString(data.industry[0].subIndustry)}
+                {data.services.length} Services
               </Text>
-            </VStack>
-            <Spacer />
-            <Text
-              color={greyColor}
-              transition={transition}
-              _groupHover={groupBlackColor}
-            >
-              Owner: {data.owner.profileName}
-            </Text>
-            <Text
-              color={greyColor}
-              transition={transition}
-              _groupHover={groupBlackColor}
-              paddingX={5}
-            >
-              {data.services.length} Services
-            </Text>
-            <Text
-              color={greyColor}
-              transition={transition}
-              _groupHover={groupBlackColor}
-            >
-              Created:
-              {" " + moment(data.creation, "YYYY-MM-DD").format("DD-MM-YYYY")}
-            </Text>
-            <VStack
-              h="100%"
-              justify="space-between"
-              alignItems="end"
-              paddingLeft={5}
-            >
-              <DashboardMenuOptions mapData={data} />
               <Text
                 color={greyColor}
                 transition={transition}
                 _groupHover={groupBlackColor}
               >
-                Modified:{" "}
-                {" " +
-                  moment(data.lastModification, "YYYY-MM-DD").format(
-                    "DD-MM-YYYY"
-                  )}
+                Created:
+                {" " + moment(data.creation, "YYYY-MM-DD").format("DD-MM-YYYY")}
               </Text>
-            </VStack>
+              <VStack
+                h="100%"
+                justify="space-between"
+                alignItems="end"
+                paddingLeft={5}
+              >
+                <DashboardMenuOptions mapData={data} />
+                <Text
+                  color={greyColor}
+                  transition={transition}
+                  _groupHover={groupBlackColor}
+                >
+                  Modified:{" "}
+                  {" " +
+                    moment(data.lastModification, "YYYY-MM-DD").format(
+                      "DD-MM-YYYY"
+                    )}
+                </Text>
+              </VStack>
+            </HStack>
           </HStack>
-        </HStack>
-      </Box>
+        </Box>
+      </NavLink>
     </Box>
   );
 }
