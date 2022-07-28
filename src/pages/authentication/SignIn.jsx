f69f279997363d14cc04b8afcdf4cb47fea4d7d8
mickael <mickael@soloso.fr>
2022-07-28 11:21:11 +0300

    Refactoring the whole file to remove useless chakra ui styling: code more readable. Add translation to the text.

--- src/pages/authentication/SignIn.jsx
+++ src/pages/authentication/SignIn.jsx
@@ -1,135 +1,83 @@
 import React from "react";
 
 import {
-  Container,
+  Flex,
   Box,
   Text,
-  Flex,
+  Center,
   Checkbox,
   Button,
   InputGroup,
   Input,
-  Stack,
+  HStack,
   Link,
   InputLeftElement,
+  Spacer,
 } from "@chakra-ui/react";
 import { Tag, Unlock } from "@styled-icons/bootstrap";
+import { useTranslation } from "react-i18next";
 
 function SignIn() {
-  return (
-    <Container maxW="container.lg" marginTop="35px">
-      <Text
-        alignSelf={"stretch"}
-        fontSize="14px"
-        color="#001011"
-        width={"564px"}
-        height={"40px"}
-        order="0"
-      >
-        Sign in with your CirclePass account to access our apps and services
-      </Text>
-      <Box>
-        <Box marginTop="20px">
-          <Text color="#001011" size="16px" lineHeight="24px">
-            Username
-          </Text>
-          <InputGroup>
-            <InputLeftElement pointerEvents="none">
-              <Tag size="20" color="#A3A3A3" />
-            </InputLeftElement>
-            <Input
-              width="90%"
-              bgColor="#ffffff"
-              type="Username"
-              placeholder="    Username"
-            />
-          </InputGroup>
-          <Text
-            marginTop="7px"
-            fontSize="12px"
-            letter-spacing="0.01em"
-            color="#718096"
-          >
-            Enter the username you have registered with
-          </Text>
-        </Box>
-        <Box marginTop="20px">
-          <Text color="#001011" size="16px" lineHeight="24px">
-            Password
-          </Text>
-          <InputGroup>
-            <InputLeftElement pointerEvents="none">
-              <Unlock color="#A3A3A3" size="20" />
-            </InputLeftElement>
-            <Input
-              width="90%"
-              bgColor="#ffffff"
-              type="Password"
-              placeholder="    Password"
-            />
-          </InputGroup>
-          <Text
-            marginTop="7px"
-            fontSize="12px"
-            letter-spacing="0.01em"
-            color="#718096"
-          >
-            Enter the password you have registered with
-          </Text>
-        </Box>
-        <Stack marginTop="15px">
-          <Checkbox marginTop={"10px"} colorScheme="blue" defaultChecked>
-            <Text fontSize={"14px"} width="204px" height="20px">
-              Remember my on this device
-            </Text>
-          </Checkbox>
-        </Stack>
-        <Stack
-          display="flex"
-          justifyContent="center"
-          spacing={4}
-          direction="row"
-          align="center"
-        >
-          <Button
-            top="30px"
-            display="flex"
-            flex-direction="row"
-            justify-content="center"
-            align-items="center"
-            padding="10px 16px"
-            gap="8px"
-            width="91px"
-            _hover={{ bg: "#00A0E9" }}
-            height="40px"
-            background="#00A0E9"
-            border-radius="6px"
-          >
-            Sign in
-          </Button>
-        </Stack>
-        {/* Some changes to be done... */}
-        <Box
-          bottom={"30px"}
-          position="absolute"
-          marginTop={"230px"}
-          marginLeft={"150px"}
-        >
-          <Text fontSize={"14px"} width={"564px"} height={"20px"}>
-            Having a problem signing in?
-          </Text>
-          <Flex flexDirection="row" display="flex">
-            <Link color="#2A69AC" fontSize="14px" width={"274px"} height="20px">
-              Forgot my username
-            </Link>
+  const { t } = useTranslation();
 
-            <Link color="#2A69AC" fontSize="14px" width={"274px"} height="20px">
-              Reset my passord
-            </Link>
-          </Flex>
-        </Box>
+  return (
+    <Flex marginX={5} flexDirection="column" h="100%">
+      <Text marginY={2.5}>
+        {t("common.authentication.sign.in.explanation")}
+      </Text>
+      <Text fontSize="md">{t("common.authentication.sign.in.username")}</Text>
+      <InputGroup marginY={2}>
+        <InputLeftElement>
+          <Tag size="20" color="grey" />
+        </InputLeftElement>
+        <Input placeholder={t("common.authentication.sign.in.username")} />
+      </InputGroup>
+      <Text color="blackAlpha.500">
+        {t("common.authentication.sign.in.username.helper")}
+      </Text>
+      <Box marginTop={4}>
+        <Text fontSize="md">
+          {t("common.authentication.sign.in.password.helper")}
+        </Text>
+        <InputGroup marginY={2}>
+          <InputLeftElement>
+            <Unlock color="grey" size="20" />
+          </InputLeftElement>
+          <Input
+            type="Password"
+            placeholder={t("common.authentication.sign.in.password.helper")}
+          />
+        </InputGroup>
+        <Text color="blackAlpha.500">
+          {t("common.authentication.sign.in.password.helper")}
+        </Text>
       </Box>
-    </Container>
+      <HStack marginTop={4}>
+        <Checkbox defaultChecked>
+          <Text>{t("common.authentication.sign.in.remember.me")}</Text>
+        </Checkbox>
+      </HStack>
+      <Center marginTop={5}>
+        <Button>{t("common.authentication.sign.in")}</Button>
+      </Center>
+      <Spacer />
+      <Center flexDirection="column">
+        <Text paddingBottom={2}>
+          {t("common.authentication.sign.in.problem")}
+        </Text>
+        <HStack>
+          <Link color="brand.500" fontSize="md">
+            {t("common.authentication.sign.in.forgot.username")}
+          </Link>
+          <Box paddingX={10}>
+            <Box w="1px" h="20px" bg="brand.500" />
+          </Box>
+          <Link color="brand.500" fontSize="md">
+            {t("common.authentication.sign.in.forgot.password")}
+          </Link>
+        </HStack>
+      </Center>
+    </Flex>
   );
 }
 
