127772c8bc975ea4e0f6b552c4759170d0d91af8
mickael <mickael@soloso.fr>
2022-07-29 10:08:05 +0300

    Add translation to the email verification component.

--- src/pages/authentication/registerSteps/steps/EmailVerification.jsx
+++ src/pages/authentication/registerSteps/steps/EmailVerification.jsx
@@ -11,31 +11,44 @@ import {
   Spacer,
 } from "@chakra-ui/react";
 import { Key } from "@styled-icons/boxicons-solid";
+import { useTranslation } from "react-i18next";
 
 function EmailVerification() {
+  const { t } = useTranslation();
   const email = "alhasan.mhd.ali@gmail.com";
 
   return (
     <Flex marginX={5} flexDirection="column" h="100%">
-      <Text>
-        Create a CirclePass account to start using our apps and services
-      </Text>
+      <Text>{t("common.authentication.register.steps.1.content.title")}</Text>
       <Text marginY={5}>
-        We have sent an email to {email}, please copy the activation code here
-        to activate your account.
+        {t("common.authentication.register.steps.2.content.subtitle.1")}
+        <span style={{ color: "#98CE00" }}> {email}</span>
+        {t("common.authentication.register.steps.2.content.subtitle.2")}
       </Text>
-      <Text>Verification Code</Text>
+      <Text>
+        {t(
+          "common.authentication.register.steps.2.content.verification.code.input.label"
+        )}
+      </Text>
       <InputGroup marginY={2}>
         <InputLeftElement>
           <Key size="20" color="grey" />
         </InputLeftElement>
-        <Input placeholder="123456" />
+        <Input
+          placeholder={t(
+            "common.authentication.register.steps.2.content.verification.code.input.placeholder"
+          )}
+        />
       </InputGroup>
       <Text color="blackAlpha.500">
-        Enter the 6-digits verification code you have received.
+        {t(
+          "common.authentication.register.steps.2.content.verification.code.input.helper"
+        )}
       </Text>
       <Center marginTop={4}>
-        <Button>Verify</Button>
+        <Button>
+          {t("common.authentication.register.steps.2.content.button")}
+        </Button>
       </Center>
       <Spacer />
     </Flex>
