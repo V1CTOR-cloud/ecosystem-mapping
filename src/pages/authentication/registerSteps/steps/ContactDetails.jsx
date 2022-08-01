3aa2a4356aa52827b3a3be99501b0c8c63d9927c
mickael <mickael@soloso.fr>
2022-08-01 11:33:16 +0300

    Implement the verification of the email and zustand state management. The backend of the email confirmation still need to be done.

--- src/pages/authentication/registerSteps/steps/ContactDetails.jsx
+++ src/pages/authentication/registerSteps/steps/ContactDetails.jsx
@@ -8,39 +8,85 @@ import {
   Input,
   Button,
   Center,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
 } from "@chakra-ui/react";
 import { Tag } from "@styled-icons/bootstrap";
 import { useTranslation } from "react-i18next";
+import isEmail from "validator/lib/isEmail";
+
+import { useStore } from "../../../../models/userStore";
 
 function ContactDetails() {
   const { t } = useTranslation();
+  const state = useStore();
+
   return (
     <Flex marginX={5} flexDirection="column" h="100%">
       <Text>{t("common.authentication.register.steps.1.content.title")}</Text>
       <Text marginY={4}>
         {t("common.authentication.register.steps.1.content.subtitle")}
       </Text>
-      <Text fontSize="md">
+      <EmailInput />
+      <Center marginTop={4}>
+        <Button
+          onClick={() => {
+            const isInvalid = !isEmail(state.email);
+            if (isInvalid) {
+              console.log("invalid");
+            } else {
+              console.log("ok");
+            }
+          }}
+        >
+          {t("common.submit")}
+        </Button>
+      </Center>
+    </Flex>
+  );
+}
+export default ContactDetails;
+
+function EmailInput() {
+  const { t } = useTranslation();
+  const state = useStore();
+  // Check every render if the email is valid and not empty to display the error message.
+  const isInvalid = !isEmail(state.email) && state.email !== "";
+
+  return (
+    <FormControl isRequired isInvalid={isInvalid}>
+      <FormLabel>
         {t("common.authentication.register.steps.1.content.email.input.label")}
-      </Text>
+      </FormLabel>
       <InputGroup marginY={2}>
         <InputLeftElement>
           <Tag size="20" color="#A3A3A3" />
         </InputLeftElement>
         <Input
+          value={state.email}
+          onChange={(e) => {
+            state.updateEmail(e.target.value);
+          }}
           placeholder={t(
             "common.authentication.register.steps.1.content.email.input.placeholder"
           )}
         />
       </InputGroup>
-      <Text color="blackAlpha.500">
-        {t("common.authentication.register.steps.1.content.email.input.helper")}
-      </Text>
-      <Center marginTop={4}>
-        <Button>{t("common.submit")}</Button>
-      </Center>
-    </Flex>
+      {isInvalid ? (
+        <FormErrorMessage>
+          {t(
+            "common.authentication.register.steps.1.content.email.input.error"
+          )}
+        </FormErrorMessage>
+      ) : (
+        <FormHelperText color="blackAlpha.500">
+          {t(
+            "common.authentication.register.steps.1.content.email.input.helper"
+          )}
+        </FormHelperText>
+      )}
+    </FormControl>
   );
 }
-
-export default ContactDetails;
