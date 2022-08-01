05664391183ea7f5cc486c4681175ea422fb1160
mickael <mickael@soloso.fr>
2022-08-01 11:57:47 +0300

    Implement the verification of the code. The backend of the code verification still need to be done.

--- src/pages/authentication/registerSteps/steps/EmailVerification.jsx
+++ src/pages/authentication/registerSteps/steps/EmailVerification.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
   InputGroup,
@@ -9,42 +9,30 @@ import {
   Text,
   Center,
   Spacer,
+  FormHelperText,
+  FormLabel,
+  FormControl,
+  FormErrorMessage,
 } from "@chakra-ui/react";
 import { Key } from "@styled-icons/boxicons-solid";
 import { useTranslation } from "react-i18next";
+import isNumeric from "validator/es/lib/isNumeric";
+
+import { useStore } from "../../../../models/userStore";
 
 function EmailVerification() {
   const { t } = useTranslation();
-  const email = "alhasan.mhd.ali@gmail.com";
+  const state = useStore();
 
   return (
     <Flex marginX={5} flexDirection="column" h="100%">
       <Text>{t("common.authentication.register.steps.1.content.title")}</Text>
       <Text marginY={5}>
         {t("common.authentication.register.steps.2.content.subtitle.1")}
-        <span style={{ color: "#98CE00" }}> {email}</span>
+        <span style={{ color: "#98CE00" }}> {state.email}</span>
         {t("common.authentication.register.steps.2.content.subtitle.2")}
       </Text>
-      <Text>
-        {t(
-          "common.authentication.register.steps.2.content.verification.code.input.label"
-        )}
-      </Text>
-      <InputGroup marginY={2}>
-        <InputLeftElement>
-          <Key size="20" color="grey" />
-        </InputLeftElement>
-        <Input
-          placeholder={t(
-            "common.authentication.register.steps.2.content.verification.code.input.placeholder"
-          )}
-        />
-      </InputGroup>
-      <Text color="blackAlpha.500">
-        {t(
-          "common.authentication.register.steps.2.content.verification.code.input.helper"
-        )}
-      </Text>
+      <VerificationCodeInput />
       <Center marginTop={4}>
         <Button>
           {t("common.authentication.register.steps.2.content.button")}
@@ -56,3 +44,48 @@ function EmailVerification() {
 }
 
 export default EmailVerification;
+
+function VerificationCodeInput() {
+  const [verificationCode, setVerificationCode] = useState("");
+  const { t } = useTranslation();
+  let isInvalid =
+    (!isNumeric(verificationCode) || verificationCode.length !== 6) &&
+    verificationCode !== "";
+
+  function handleInputChange(e) {
+    setVerificationCode(e.target.value);
+  }
+
+  return (
+    <FormControl isRequired isInvalid={isInvalid}>
+      <FormLabel>
+        {t(
+          "common.authentication.register.steps.2.content.verification.code.input.label"
+        )}
+      </FormLabel>
+      <InputGroup marginY={2}>
+        <InputLeftElement>
+          <Key size="20" color="grey" />
+        </InputLeftElement>
+        <Input
+          value={verificationCode}
+          onChange={handleInputChange}
+          placeholder={t(
+            "common.authentication.register.steps.2.content.verification.code.input.placeholder"
+          )}
+        />
+      </InputGroup>
+      {isInvalid ? (
+        <FormErrorMessage>
+          Please enter a 6-digits code sent at your email address.
+        </FormErrorMessage>
+      ) : (
+        <FormHelperText color="blackAlpha.500">
+          {t(
+            "common.authentication.register.steps.2.content.verification.code.input.helper"
+          )}
+        </FormHelperText>
+      )}
+    </FormControl>
+  );
+}
