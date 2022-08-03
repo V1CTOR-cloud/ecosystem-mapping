f5b979ffdf3216c10f955eeab9a4385f80411de3
mickael <mickael@soloso.fr>
2022-08-03 13:56:41 +0300

    Add devtool to the userStore.

--- src/models/userStore.js
+++ src/models/userStore.js
@@ -1,17 +1,20 @@
 import createContext from "zustand/context";
 import create from "zustand";
+import { devtools } from "zustand/middleware";
 
 export const { Provider, useStore } = createContext();
 
 export const userStore = () =>
-  create((set) => ({
-    firstName: "",
-    lastName: "",
-    email: "",
-    username: "",
+  create(
+    devtools((set) => ({
+      firstName: "",
+      lastName: "",
+      email: "",
+      username: "",
 
-    updateEmail: (email) => set({ email: email }),
-    updateFirstName: (firstName) => set({ firstName: firstName }),
-    updateLastName: (lastName) => set({ lastName: lastName }),
-    updateUsername: (username) => set({ username: username }),
-  }));
+      updateEmail: (email) => set({ email: email }),
+      updateFirstName: (firstName) => set({ firstName: firstName }),
+      updateLastName: (lastName) => set({ lastName: lastName }),
+      updateUsername: (username) => set({ username: username }),
+    }))
+  );
