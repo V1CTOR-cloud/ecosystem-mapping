23b44aa3682a45eb3986a8e41a5e7d50a91f0c58
mickael <mickael@soloso.fr>
2022-08-03 13:58:52 +0300

    Add formik for email verification and state management + Creation of the account in AWS on submission.

--- src/pages/authentication/registerSteps/steps/ContactDetails.jsx
+++ src/pages/authentication/registerSteps/steps/ContactDetails.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import {
   Text,
@@ -16,12 +16,29 @@ import {
 import { Tag } from "@styled-icons/bootstrap";
 import { useTranslation } from "react-i18next";
 import isEmail from "validator/lib/isEmail";
+import { Field, Form, Formik } from "formik";
 
+import { EmailCreation } from "../../../../service/cognitoAuth";
 import { useStore } from "../../../../models/userStore";
+import { TabsContext } from "../Steps";
 
 function ContactDetails() {
-  const { t } = useTranslation();
+  const tabsContext = useContext(TabsContext);
   const state = useStore();
+  const { t } = useTranslation();
+
+  function validateEmail(value) {
+    return !isEmail(value) && value !== "";
+  }
+
+  async function onSubmit(values) {
+    const res = await EmailCreation(values, state);
+
+    // The account was created, we pass to the next steps
+    if (res) {
+      tabsContext[1](1);
+    }
+  }
 
   return (
     <Flex marginX={5} flexDirection="column" h="100%">
@@ -29,64 +46,64 @@ function ContactDetails() {
       <Text marginY={4}>
         {t("common.authentication.register.steps.1.content.subtitle")}
       </Text>
-      <EmailInput />
-      <Center marginTop={4}>
-        <Button
-          onClick={() => {
-            const isInvalid = !isEmail(state.email);
-            if (isInvalid) {
-              console.log("invalid");
-            } else {
-              console.log("ok");
-            }
-          }}
-        >
-          {t("common.submit")}
-        </Button>
-      </Center>
+      <Formik
+        initialValues={{ email: "" }}
+        onSubmit={(values) => onSubmit(values)}
+      >
+        {() => (
+          <Form>
+            <Field name="email" validate={validateEmail}>
+              {({ field, form }) => {
+                const isButtonDisable =
+                  form.values.email === "" || !isEmail(form.values.email);
+
+                return (
+                  <>
+                    <FormControl isRequired isInvalid={form.errors.email}>
+                      <FormLabel>
+                        {t(
+                          "common.authentication.register.steps.1.content.email.input.label"
+                        )}
+                      </FormLabel>
+                      <InputGroup marginY={2}>
+                        <InputLeftElement>
+                          <Tag size="20" color="#A3A3A3" />
+                        </InputLeftElement>
+                        <Input
+                          {...field}
+                          placeholder={t(
+                            "common.authentication.register.steps.1.content.email.input.placeholder"
+                          )}
+                        />
+                      </InputGroup>
+                      {form.errors.email ? (
+                        <FormErrorMessage>
+                          {t(
+                            "common.authentication.register.steps.1.content.email.input.error"
+                          )}
+                        </FormErrorMessage>
+                      ) : (
+                        <FormHelperText color="blackAlpha.500">
+                          {t(
+                            "common.authentication.register.steps.1.content.email.input.helper"
+                          )}
+                        </FormHelperText>
+                      )}
+                    </FormControl>
+                    <Center marginTop={4}>
+                      <Button type="submit" isDisabled={isButtonDisable}>
+                        {t("common.submit")}
+                      </Button>
+                    </Center>
+                  </>
+                );
+              }}
+            </Field>
+          </Form>
+        )}
+      </Formik>
     </Flex>
   );
 }
+
 export default ContactDetails;
-
-function EmailInput() {
-  const { t } = useTranslation();
-  const state = useStore();
-  // Check every render if the email is valid and not empty to display the error message.
-  const isInvalid = !isEmail(state.email) && state.email !== "";
-
-  return (
-    <FormControl isRequired isInvalid={isInvalid}>
-      <FormLabel>
-        {t("common.authentication.register.steps.1.content.email.input.label")}
-      </FormLabel>
-      <InputGroup marginY={2}>
-        <InputLeftElement>
-          <Tag size="20" color="#A3A3A3" />
-        </InputLeftElement>
-        <Input
-          value={state.email}
-          onChange={(e) => {
-            state.updateEmail(e.target.value);
-          }}
-          placeholder={t(
-            "common.authentication.register.steps.1.content.email.input.placeholder"
-          )}
-        />
-      </InputGroup>
-      {isInvalid ? (
-        <FormErrorMessage>
-          {t(
-            "common.authentication.register.steps.1.content.email.input.error"
-          )}
-        </FormErrorMessage>
-      ) : (
-        <FormHelperText color="blackAlpha.500">
-          {t(
-            "common.authentication.register.steps.1.content.email.input.helper"
-          )}
-        </FormHelperText>
-      )}
-    </FormControl>
-  );
-}
