afa7043e50390dbe8f5d05420c7ed068327f89b7
mickael <mickael@soloso.fr>
2022-08-05 13:39:50 +0300

    Create sign in function, update user info and update the others functions.

--- src/service/cognitoAuth.js
+++ src/service/cognitoAuth.js
@@ -1,7 +1,8 @@
 import * as AmazonCognitoIdentity from "amazon-cognito-identity-js";
 import { CognitoUserPool } from "amazon-cognito-identity-js";
-import ToastComponent from "../components/basic/ToastComponent";
 import uuid from "react-uuid";
+
+import ToastComponent from "../components/basic/ToastComponent";
 
 const { REACT_APP_COGNITO_USER_POOLS_ID, REACT_APP_COGNITO_CLIENT_ID } =
   process.env;
@@ -13,17 +14,17 @@ const poolData = {
 
 const userPool = new CognitoUserPool(poolData);
 let cognitoUser;
+let password = "";
 
-// Create a new user in AWS Cognito.
-// Create dummy data for first name, last name, username and password for later update them.
 /**
  * Create a new user in AWS Cognito.
  * Create dummy data for first name, last name, username and password for later update them.
  * @param values {Object} - The form values from the formik component, here only the email.
- * @param state - The Zustand state of the userStore.
+ * @param updateEmail - The updateEmail Zustand function of the userStore.
+ * @param updateUsername - The updateUsername Zustand function of the userStore.
  * @return {Promise<Boolean>} - True if the user was created, otherwise a toast is displayed.
  */
-export async function EmailCreation(values, state) {
+export async function accountCreation(values, updateEmail, updateUsername) {
   const attributes = [
     new AmazonCognitoIdentity.CognitoUserAttribute({
       Name: "email",
@@ -39,18 +40,28 @@ export async function EmailCreation(values, state) {
     }),
   ];
 
-  return new Promise((resolve, reject) => {
-    userPool.signUp(uuid(), "Azerty123()", attributes, null, (err, result) => {
-      if (err) {
-        ToastComponent(err.message, "error", 10000);
-        reject(err);
-        return;
-      }
+  password = values.password;
 
-      cognitoUser = result.user;
-      state.updateEmail(values.email);
-      resolve(true);
-    });
+  return new Promise((resolve, reject) => {
+    userPool.signUp(
+      values.username,
+      values.password,
+      attributes,
+      null,
+      (err, result) => {
+        if (err) {
+          ToastComponent(err.message, "error", 10000);
+          reject(err);
+          return;
+        }
+
+        // Update variables in the userStore and cognitoUser also.
+        cognitoUser = result.user;
+        updateEmail(values.email);
+        updateUsername(values.username);
+        resolve(true);
+      }
+    );
   });
 }
 
@@ -59,7 +70,7 @@ export async function EmailCreation(values, state) {
  * @param values - The form values from the formik component, here only the code.
  * @return {Promise<Boolean>} - True if the code is correct, otherwise a toast is displayed.
  */
-export function EmailCodeVerification(values) {
+export function emailCodeVerification(values) {
   return new Promise((resolve, reject) => {
     cognitoUser.confirmRegistration(values.code, true, (err) => {
       if (err) {
@@ -68,6 +79,72 @@ export function EmailCodeVerification(values) {
         return;
       }
       resolve(true);
+    });
+  });
+}
+
+/**
+ * Update the family name and given name of the user.
+ * @param values The form values from the formik component (firstName and lastName).
+ * @return {Promise<unknown>} - True if the user was updated, otherwise a toast is displayed.
+ */
+export function updateUserInfo(values) {
+  const attributes = [
+    new AmazonCognitoIdentity.CognitoUserAttribute({
+      Name: "given_name",
+      Value: values.firstName,
+    }),
+    new AmazonCognitoIdentity.CognitoUserAttribute({
+      Name: "family_name",
+      Value: values.lastName,
+    }),
+  ];
+
+  return new Promise((resolve, reject) => {
+    cognitoUser.updateAttributes(attributes, function (err, result) {
+      if (err) {
+        alert(err.message || JSON.stringify(err));
+        reject(err);
+        return;
+      }
+      resolve(true);
+    });
+  });
+}
+
+/**
+ * Sign in the user with the username and password.
+ * @param values The form values from the formik component (username and password).
+ * @return {Promise<unknown>} - True if the user was signed in, otherwise a toast is displayed.
+ */
+export function signIn(values) {
+  const authenticationData = {
+    Username: values.username,
+    Password: values.password ? values.password : password,
+  };
+
+  const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(
+    authenticationData
+  );
+
+  const userData = {
+    Username: values.username,
+    Pool: userPool,
+  };
+
+  const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+
+  return new Promise((resolve, reject) => {
+    cognitoUser.authenticateUser(authenticationDetails, {
+      onSuccess: function (result) {
+        console.log("success", result);
+        resolve(true);
+      },
+
+      onFailure: function (err) {
+        ToastComponent(err.message, "error", 10000);
+        reject(err);
+      },
     });
   });
 }
