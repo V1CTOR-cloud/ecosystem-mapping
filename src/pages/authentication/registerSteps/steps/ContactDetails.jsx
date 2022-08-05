6e0f89c63d75f3eb2a011956b5964a4ed33c002b
mickael <mickael@soloso.fr>
2022-08-05 10:06:14 +0300

    Move username and password to the step 1.

--- src/pages/authentication/registerSteps/steps/ContactDetails.jsx
+++ src/pages/authentication/registerSteps/steps/ContactDetails.jsx
@@ -1,26 +1,18 @@
 import React, { useContext } from "react";
 
-import {
-  Text,
-  Flex,
-  InputLeftElement,
-  InputGroup,
-  Input,
-  Button,
-  Center,
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-  FormHelperText,
-} from "@chakra-ui/react";
-import { Tag } from "@styled-icons/bootstrap";
+import { Text, Flex, Button, Center, Box } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
 import isEmail from "validator/lib/isEmail";
 import { Field, Form, Formik } from "formik";
+import { Tag } from "@styled-icons/bootstrap";
+import { Email } from "@styled-icons/material-outlined";
 
-import { EmailCreation } from "../../../../service/cognitoAuth";
+import { accountCreation } from "../../../../service/cognitoAuth";
 import { useStore } from "../../../../models/userStore";
 import { TabsContext } from "../Steps";
+import AuthInput from "../../../../components/authentication/AuthInput";
+import isStrongPassword from "validator/es/lib/isStrongPassword";
+import { Unlock } from "@styled-icons/feather";
 
 function ContactDetails() {
   const tabsContext = useContext(TabsContext);
@@ -31,8 +23,16 @@ function ContactDetails() {
     return !isEmail(value) && value !== "";
   }
 
+  function validateUsername(value) {
+    return value.length <= 5 && value !== "";
+  }
+
+  function validatePassword(value) {
+    return !isStrongPassword(value) && value !== "";
+  }
+
   async function onSubmit(values) {
-    const res = await EmailCreation(values, state);
+    const res = await accountCreation(values, state);
 
     // The account was created, we pass to the next steps
     if (res) {
@@ -47,60 +47,104 @@ function ContactDetails() {
         {t("common.authentication.register.steps.1.content.subtitle")}
       </Text>
       <Formik
-        initialValues={{ email: "" }}
+        initialValues={{ email: "", username: "", password: "" }}
         onSubmit={(values) => onSubmit(values)}
       >
-        {() => (
-          <Form>
-            <Field name="email" validate={validateEmail}>
-              {({ field, form }) => {
-                const isButtonDisable =
-                  form.values.email === "" || !isEmail(form.values.email);
+        {({ values }) => {
+          const isUsernameInvalid =
+            values.username === "" || values.username.length <= 5;
+          const isEmailInvalid = values.email === "" || !isEmail(values.email);
+          const isPasswordInvalid =
+            values.password === "" || !isStrongPassword(values.password);
 
-                return (
-                  <>
-                    <FormControl isRequired isInvalid={form.errors.email}>
-                      <FormLabel>
-                        {t(
+          const isButtonDisable =
+            isEmailInvalid || isUsernameInvalid || isPasswordInvalid;
+
+          return (
+            <Form>
+              <Field name="username" validate={validateUsername}>
+                {({ field, form }) => {
+                  return (
+                    <AuthInput
+                      field={field}
+                      form={form}
+                      helper={t(
+                        "common.authentication.register.steps.3.content.username.input.helper"
+                      )}
+                      label={t(
+                        "common.authentication.register.steps.3.content.username.input.label"
+                      )}
+                      placeholder={t(
+                        "common.authentication.register.steps.3.content.username.input.label"
+                      )}
+                      error={t(
+                        "common.authentication.register.steps.3.content.username.input.error"
+                      )}
+                      validation={form.errors.username}
+                      icon={<Tag size="20" color="#A3A3A3" />}
+                    />
+                  );
+                }}
+              </Field>
+              <Box marginY={3}>
+                <Field name="email" validate={validateEmail}>
+                  {({ field, form }) => {
+                    return (
+                      <AuthInput
+                        field={field}
+                        form={form}
+                        helper={t(
+                          "common.authentication.register.steps.1.content.email.input.helper"
+                        )}
+                        label={t(
                           "common.authentication.register.steps.1.content.email.input.label"
                         )}
-                      </FormLabel>
-                      <InputGroup marginY={2}>
-                        <InputLeftElement>
-                          <Tag size="20" color="#A3A3A3" />
-                        </InputLeftElement>
-                        <Input
-                          {...field}
-                          placeholder={t(
-                            "common.authentication.register.steps.1.content.email.input.placeholder"
-                          )}
-                        />
-                      </InputGroup>
-                      {form.errors.email ? (
-                        <FormErrorMessage>
-                          {t(
-                            "common.authentication.register.steps.1.content.email.input.error"
-                          )}
-                        </FormErrorMessage>
-                      ) : (
-                        <FormHelperText color="blackAlpha.500">
-                          {t(
-                            "common.authentication.register.steps.1.content.email.input.helper"
-                          )}
-                        </FormHelperText>
+                        placeholder={t(
+                          "common.authentication.register.steps.1.content.email.input.placeholder"
+                        )}
+                        error={t(
+                          "common.authentication.register.steps.1.content.email.input.error"
+                        )}
+                        validation={form.errors.email}
+                        icon={<Email size="20" color="#A3A3A3" />}
+                      />
+                    );
+                  }}
+                </Field>
+              </Box>
+              <Field name="password" validate={validatePassword}>
+                {({ field, form }) => {
+                  return (
+                    <AuthInput
+                      field={field}
+                      form={form}
+                      helper={t(
+                        "common.authentication.register.steps.3.content.password.input.helper"
                       )}
-                    </FormControl>
-                    <Center marginTop={4}>
-                      <Button type="submit" isDisabled={isButtonDisable}>
-                        {t("common.submit")}
-                      </Button>
-                    </Center>
-                  </>
-                );
-              }}
-            </Field>
-          </Form>
-        )}
+                      label={t(
+                        "common.authentication.register.steps.3.content.password.input.label"
+                      )}
+                      placeholder={t(
+                        "common.authentication.register.steps.3.content.password.input.label"
+                      )}
+                      error={t(
+                        "common.authentication.register.steps.3.content.password.input.error"
+                      )}
+                      validation={form.errors.password}
+                      icon={<Unlock size="20" color="grey" />}
+                      type={"password"}
+                    />
+                  );
+                }}
+              </Field>
+              <Center marginTop={4}>
+                <Button type="submit" isDisabled={isButtonDisable}>
+                  {t("common.submit")}
+                </Button>
+              </Center>
+            </Form>
+          );
+        }}
       </Formik>
     </Flex>
   );
