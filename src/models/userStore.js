36073ecbaa2f52c3e0a68fae11a6ff001691d002
mickael <mickael@soloso.fr>
2022-08-08 12:33:01 +0300

    Create the provider with the store directly instead of passing it as props.

--- src/models/userStore.js
+++ src/models/userStore.js
@@ -1,20 +1,45 @@
+import React from "react";
+
 import createContext from "zustand/context";
 import create from "zustand";
 import { devtools } from "zustand/middleware";
+import PropTypes from "prop-types";
 
 export const { Provider, useStore } = createContext();
 
-export const userStore = () =>
-  create(
-    devtools((set) => ({
-      firstName: "",
-      lastName: "",
-      email: "",
-      username: "",
+// Provider that will contain all the information of the user.
+export const UserProvider = ({ children }) => {
+  return (
+    <Provider
+      createStore={() =>
+        create(
+          devtools((set) => ({
+            firstName: "",
+            lastName: "",
+            email: "",
+            username: "",
+            isLoggedIn: false,
 
-      updateEmail: (email) => set((state) => ({ email: email })),
-      updateName: (firstName) => set((state) => ({ firstName: firstName })),
-      updateUsername: (username) => set((state) => ({ username: username })),
-      updateLastName: (lastName) => set((state) => ({ lastName: lastName })),
-    }))
+            updateEmail: (email) => set((state) => ({ email: email })),
+            updateFirstName: (firstName) =>
+              set((state) => ({ firstName: firstName })),
+            updateUsername: (username) =>
+              set((state) => ({ username: username })),
+            updateLastName: (lastName) =>
+              set((state) => ({ lastName: lastName })),
+            updateIsLoggedIn: (bool) => set((state) => ({ isLoggedIn: bool })),
+          }))
+        )
+      }
+    >
+      {children}
+    </Provider>
   );
+};
+
+UserProvider.propTypes = {
+  /**
+   * The children of the UserProvider.
+   */
+  children: PropTypes.element.isRequired,
+};
