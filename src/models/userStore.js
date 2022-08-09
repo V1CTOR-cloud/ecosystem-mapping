e750e23746f916fc9e99c45c11af0f9676b6b8ea
mickael <mickael@soloso.fr>
2022-08-09 10:36:22 +0300

    Add persist to the userStore

--- src/models/userStore.js
+++ src/models/userStore.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import createContext from "zustand/context";
 import create from "zustand";
-import { devtools } from "zustand/middleware";
+import { devtools, persist } from "zustand/middleware";
 import PropTypes from "prop-types";
 
 export const { Provider, useStore } = createContext();
@@ -13,22 +13,35 @@ export const UserProvider = ({ children }) => {
     <Provider
       createStore={() =>
         create(
-          devtools((set) => ({
-            firstName: "",
-            lastName: "",
-            email: "",
-            username: "",
-            isLoggedIn: false,
+          devtools(
+            persist((set) => ({
+              firstName: "",
+              lastName: "",
+              email: "",
+              username: "",
+              isLoggedIn: false,
 
-            updateEmail: (email) => set((state) => ({ email: email })),
-            updateFirstName: (firstName) =>
-              set((state) => ({ firstName: firstName })),
-            updateUsername: (username) =>
-              set((state) => ({ username: username })),
-            updateLastName: (lastName) =>
-              set((state) => ({ lastName: lastName })),
-            updateIsLoggedIn: (bool) => set((state) => ({ isLoggedIn: bool })),
-          }))
+              updateEmail: (email) => set((state) => ({ email: email })),
+              updateFirstName: (firstName) =>
+                set((state) => ({ firstName: firstName })),
+              updateUsername: (username) =>
+                set((state) => ({ username: username })),
+              updateLastName: (lastName) =>
+                set((state) => ({ lastName: lastName })),
+              updateIsLoggedIn: (bool) =>
+                set((state) => ({ isLoggedIn: bool })),
+
+              // Clear the user when logging out mainly.
+              clearUser: () =>
+                set((state) => ({
+                  firstName: "",
+                  lastName: "",
+                  email: "",
+                  username: "",
+                  isLoggedIn: false,
+                })),
+            }))
+          )
         )
       }
     >
