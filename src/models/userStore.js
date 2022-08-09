354b0e43adc471c8379f31cb538a4a411aacbae8
mickael <mickael@soloso.fr>
2022-08-09 12:45:22 +0300

    Move all cognitoAuth functions inside the userStore, also remove the cognitoAuth file.

--- src/models/userStore.js
+++ src/models/userStore.js
@@ -4,42 +4,159 @@ import createContext from "zustand/context";
 import create from "zustand";
 import { devtools, persist } from "zustand/middleware";
 import PropTypes from "prop-types";
+import { Auth } from "aws-amplify";
+import uuid from "react-uuid";
+
+import ToastComponent from "../components/basic/ToastComponent";
 
 export const { Provider, useStore } = createContext();
 
-// Provider that will contain all the information of the user.
+let password = "";
+
+// Provider that will contain all the information of the user, in addition of all the function to update it.
 export const UserProvider = ({ children }) => {
   return (
     <Provider
       createStore={() =>
         create(
           devtools(
-            persist((set) => ({
+            persist((set, get) => ({
+              id: "",
               firstName: "",
               lastName: "",
               email: "",
               username: "",
               isLoggedIn: false,
 
-              updateEmail: (email) => set((state) => ({ email: email })),
-              updateFirstName: (firstName) =>
-                set((state) => ({ firstName: firstName })),
-              updateUsername: (username) =>
-                set((state) => ({ username: username })),
-              updateLastName: (lastName) =>
-                set((state) => ({ lastName: lastName })),
+              /**
+               * Function to set the isLoggedIn state.
+               * @param bool - Boolean value that will be set to the isLoggedIn state.
+               */
               updateIsLoggedIn: (bool) =>
                 set((state) => ({ isLoggedIn: bool })),
 
-              // Clear the user when logging out mainly.
-              clearUser: () =>
-                set((state) => ({
-                  firstName: "",
-                  lastName: "",
-                  email: "",
-                  username: "",
-                  isLoggedIn: false,
-                })),
+              /**
+               * Log out the user and clear the user properties.
+               * @return {Promise<void>} - Void is successful, otherwise a toast is displayed.
+               */
+              logOut: async () => {
+                try {
+                  await Auth.signOut();
+                  // Set the user properties to default values (empty).
+                  set((state) => ({
+                    id: "",
+                    firstName: "",
+                    lastName: "",
+                    email: "",
+                    username: "",
+                    isLoggedIn: false,
+                  }));
+                } catch (err) {
+                  ToastComponent(err.message, "error", 10000);
+                }
+              },
+
+              /**
+               * Create a new user in AWS Cognito.
+               * Create dummy data for first name and last name to update them later.
+               * @param values - The form values from the formik component, here only the email.
+               * @return {Promise<boolean>} - True if the user was created, otherwise a toast is displayed.
+               */
+              createAccount: async (values) => {
+                try {
+                  const attributes = {
+                    username: values.username,
+                    password: values.password,
+                    attributes: {
+                      email: values.email,
+                      family_name: uuid(),
+                      given_name: uuid(),
+                    },
+                  };
+                  password = values.password;
+
+                  const res = await Auth.signUp(attributes);
+
+                  // Set the user properties to new ones.
+                  set((state) => ({
+                    id: res.userSub,
+                    email: values.email,
+                    username: values.username,
+                  }));
+
+                  return true;
+                } catch (err) {
+                  ToastComponent(err.message, "error", 10000);
+                }
+              },
+
+              /**
+               * Log in the user with the username and password.
+               * @param values - The form values from the formik component (username and password).
+               * @param isRegistration - True if the user is registering, false if the user is logging in.
+               * @return {Promise<boolean>} - True if the user was signed in, otherwise a toast is displayed.
+               */
+              signIn: async (values, isRegistration) => {
+                try {
+                  const res = await Auth.signIn(
+                    values.username,
+                    values.password ? values.password : password
+                  );
+
+                  password = "";
+
+                  set((state) => ({
+                    id: res.attributes.sub,
+                    firstName: res.attributes.given_name,
+                    lastName: res.attributes.family_name,
+                    email: res.attributes.email,
+                    username: res.username,
+                    isLoggedIn: !isRegistration,
+                  }));
+
+                  return true;
+                } catch (err) {
+                  ToastComponent(err.message, "error", 10000);
+                }
+              },
+
+              /**
+               * Verify the code sent to the user by email and set the account to confirmed in Cognito if correct.
+               * @param values - The form values from the formik component, here only the code.
+               * @return {Promise<void>} If error a toast is displayed.
+               */
+              emailCodeVerification: async (values) => {
+                try {
+                  await Auth.confirmSignUp(get().username, values.code);
+                } catch (err) {
+                  ToastComponent(err.message, "error", 10000);
+                }
+              },
+              /**
+               * Update the family name and given name of the user in AWS Cognito.
+               * @param values The form values from the formik component (firstName and lastName).
+               * @return {Promise<boolean>} - True if the user was updated, otherwise a toast is displayed.
+               */
+              updateUserInfo: async (values) => {
+                try {
+                  const user = await Auth.currentAuthenticatedUser();
+                  const attributes = {
+                    family_name: values.lastName,
+                    given_name: values.firstName,
+                  };
+
+                  await Auth.updateUserAttributes(user, attributes);
+
+                  set((state) => ({
+                    firstName: values.firstName,
+                    lastName: values.lastName,
+                  }));
+
+                  return true;
+                } catch (err) {
+                  ToastComponent(err.message, "error", 10000);
+                }
+              },
             }))
           )
         )
